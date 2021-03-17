f27ca8ccfd8e2784807d98ec077ec83b803ca281
Krutik Gdhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-03-17 22:17:28 +0530

    Add files via upload

---
+++ client/src/components/BarChart.js
@@ -0,0 +1,51 @@
+import { Bar, Line, Pie, Polar, Radar } from 'react-chartjs-2';
+
+const BarChart = () => {
+    return (
+        <div>
+            <Bar 
+                data={{
+                    labels: [2016, 2017, 2018, 2019, 2020, 2021],
+                    datasets: [
+                        {
+                          label: 'No of Events',
+                          data: [12, 10, 2, 3, 5, 15],
+                          backgroundColor: [
+                            'rgba(255, 99, 132, 0.5)',
+                            'rgba(54, 162, 235, 0.5)',
+                            'rgba(255, 206, 86, 0.5)',
+                            'rgba(75, 192, 192, 0.5)',
+                            'rgba(153, 102, 255, 0.5)',
+                            'rgba(255, 159, 64, 0.5)',
+                          ],
+                          borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                          ],
+                          borderWidth: 1,
+                        },
+                      ],
+                    }
+                }
+                height={400}
+                width={600}
+                options={{
+                    maintainAspectRatio: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                            },
+                        },],
+                    },
+                }}
+            />
+        </div>
+    )
+}
+
+export default BarChart;
